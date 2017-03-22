2f02c65d0250acae4f2156c6c4ff652bfba18300
Stark <cedric.stark@tum.de>
2017-03-22 18:36:51 +0100

    integrating still gives access violation

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <OpenSim/OpenSim.h>
+#include <iostream>
 
 using namespace OpenSim;
 using namespace SimTK;
@@ -7,26 +7,41 @@ using namespace SimTK;
 int main(int argc, char* argv[]) {
 	try {
 		//replace by input
-		std::string path("E:/Dokumente/Schule/tum/Informatik/6/Bachelor-Arbeit/Original Files - UpperExtremityModel/Stanford VA upper limb model_0.osim");
-		
+		std::string path("E:/Dokumente/Schule/tum/Informatik/6/Bachelor-Arbeit/model_works/MoBL_ARMS_Upper_Limb_Model_OpenSim.osim");
+
 		Model model(path);
-		Set<Muscle> muscles = model.getMuscles();
+		State &si = model.initSystem();
+		std::cout << model.getName() << std::endl;
+		int b = model.getNumBodies();
+		int c = model.getNumControls();
+		std::cout << b << std::endl;
+		std::cout << c << std::endl;
+		std::cout << "loaded" << std::endl;
 
 		//----------------
 		PrescribedController *controla = new PrescribedController;
 		controla->setActuators(model.updActuators());
 
-		for (int i = 0; i < muscles.getSize(); i++) {
-			controla->prescribeControlForActuator(muscles.get(i).getName(), new Constant(1.0));
+		for (int i = 0; i < c; i++) {
+			//controla->prescribeControlForActuator(i, new Constant(1.0));
 		}
+
+		std::cout << "excited" << std::endl;
 
 		model.addController(controla);
 
+		std::cout << "added" << std::endl;
+
+
 		//----------------
-		State si = model.initSystem();
-		model.updMultibodySystem().realize(si, Stage::Dynamics);
-		RungeKutta2Integrator integrator(model.getMultibodySystem());
+		//model.updMultibodySystem().realize(si, Stage::Dynamics);
+		std::cout << "realized" << std::endl;
+		RungeKuttaMersonIntegrator integrator(model.getMultibodySystem());
+		std::cout << "integrate" << std::endl;
 		integrator.setAccuracy(10e-6);
+		std::cout << "accurate" << std::endl;
+
+		std::cout << "integrator" << std::endl;
 
 		//----------------
 		Manager benson(model, integrator);
@@ -36,10 +51,18 @@ int main(int argc, char* argv[]) {
 		benson.setInitialTime(starttime);
 		benson.setFinalTime(finaltime);
 
-		benson.integrate(si);
+		std::cout << "managed" << std::endl;
+
+		try {
+			benson.integrate(si);
+		}	catch (...) {
+			std::cout << "bruh" << std::endl;
+		}
+
+		std::cout << "integrated" << std::endl;
 
 		benson.getStateStorage().print("states.sto");
-		//filter angles and return them
+		////filter angles and return them
 
 	}
 	catch (OpenSim::Exception ex)
