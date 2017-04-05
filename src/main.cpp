f3fed22c8c3ac4fec8d34399349db12caaacf4f3
Stark <cedric.stark@tum.de>
2017-04-05 21:02:04 +0200

    now using forward tool instead of manager, different controls and different initial states

--- src/main.cpp
+++ src/main.cpp
@@ -8,198 +8,52 @@ using namespace OpenSim;
 using namespace SimTK;
 
 int main(int argc, char* argv[]) {
-	//manager.integrate() crashing
-	//try {
-	//	//replace by input
-	//	std::string path("E:/Dokumente/Schule/tum/Informatik/6/Bachelor-Arbeit/MoBL_ARMS_OpenSim33tutorial/ModelFiles/MoBL_ARMS_module6_7_CMC.osim");
+	try {
+		std::string path("E:/Dokumente/Schule/tum/Informatik/6/Bachelor-Arbeit/MoBL_ARMS_OpenSim33tutorial/MoBL_ARMS_tutorial_33/MoBL-ARMS OpenSim tutorial_33/ModelFiles/MoBL_ARMS_module6_7_CMC.osim");
+		Model model(path);
 
-	//	Model model(path);
-	//	State &si = model.initSystem();
-	//	std::cout << model.getName() << std::endl;
-	//	int b = model.getNumBodies();
-	//	int c = model.getNumControls();
-	//	std::cout << b << std::endl;
-	//	std::cout << c << std::endl;
-	//	std::cout << "loaded" << std::endl;
+		//simulation from 0.62 to 0.72
 
-	//	//----------------
-	//	PrescribedController *controla = new PrescribedController;
-	//	controla->setActuators(model.updActuators());
+		//standard controls from download
+		ForwardTool *tool = new ForwardTool("E:/Dokumente/Schule/tum/Informatik/6/Bachelor-Arbeit/MoBL_ARMS_OpenSim33tutorial/MoBL_ARMS_tutorial_33/MoBL-ARMS OpenSim tutorial_33/ModelFiles/InputFiles_module7_CMCDrivenFowardSimulation/MoBL_ARMS_module7_FDS_Setup - Kopie.xml");
+		tool->setModel(model);
+		std::clock_t startTime = std::clock();
+		tool->run();
+		auto t1 = 1.e3*(std::clock() - startTime) / CLOCKS_PER_SEC;
+		
+		//with a different control file
+		startTime = std::clock();
+		tool->setControlsFileName("E:/Dokumente/Schule/tum/Informatik/6/Bachelor-Arbeit/MoBL_ARMS_OpenSim33tutorial/MoBL_ARMS_tutorial_33/MoBL-ARMS OpenSim tutorial_33/ModelFiles/InputFiles_module7_CMCDrivenFowardSimulation/controls.xml");
+		tool->addAnalysisSetToModel();
+		tool->run();
+		auto t2 = 1.e3*(std::clock() - startTime) / CLOCKS_PER_SEC;
 
-	//	for (int i = 0; i < c; i++) {
-	//		controla->prescribeControlForActuator(i, new Constant(1.0));
-	//	}
+		//with different first states file
+		startTime = std::clock();
+		tool->setStatesFileName("E:/Dokumente/Schule/tum/Informatik/6/Bachelor-Arbeit/MoBL_ARMS_OpenSim33tutorial/MoBL_ARMS_tutorial_33/MoBL-ARMS OpenSim tutorial_33/ModelFiles/InputFiles_module7_CMCDrivenFowardSimulation/CMC_Reach8_states_abbrev - Kopie.sto");
+		tool->run();
+		auto t3 = 1.e3*(std::clock() - startTime) / CLOCKS_PER_SEC;
+		//command line, standard controls
+		startTime = std::clock();
+		system("cd E:/Dokumente/Schule/tum/Informatik/6/Bachelor-Arbeit/MoBL_ARMS_OpenSim33tutorial/MoBL_ARMS_tutorial_33/MoBL-ARMS OpenSim tutorial_33/ModelFiles/InputFiles_module7_CMCDrivenFowardSimulation && forward -S \"MoBL_ARMS_module7_FDS_Setup - Kopie - Kopie.xml\"");
+		auto t4 = 1.e3*(std::clock() - startTime) / CLOCKS_PER_SEC;
+		
 
-	//	std::cout << "excited" << std::endl;
-
-	//	model.addController(controla);
-
-	//	std::cout << "added" << std::endl;
-
-
-	//	//----------------
-	//	model.updMultibodySystem().realize(si, Stage::Dynamics);
-	//	std::cout << "realized" << std::endl;
-	//	RungeKuttaMersonIntegrator integrator(model.getMultibodySystem());
-	//	std::cout << "integrate" << std::endl;
-	//	integrator.setAccuracy(10e-6);
-	//	std::cout << "accurate" << std::endl;
-
-	//	std::cout << "integrator" << std::endl;
-
-	//	//----------------
-	//	Manager benson(model, integrator);
-	//	double starttime = 0.0;
-	//	double finaltime = 0.001;
-	//	
-	//	benson.setInitialTime(starttime);
-	//	benson.setFinalTime(finaltime);
-
-	//	std::cout << "managed" << std::endl;
-
-	//	try {
-	//		benson.integrate(si);
-	//	}	catch (...) {
-	//		std::cout << "bruh" << std::endl;
-	//	}
-
-	//	std::cout << "integrated" << std::endl;
-
-	//	benson.getStateStorage().print("states.sto");
-	//	////filter angles and return them
-
-	//}
-	//catch (OpenSim::Exception ex)
-	//{
-	//	std::cout << "opensim " << ex.getMessage() << std::endl;
-	//	return 1;
-	//}
-	//catch (std::exception ex)
-	//{
-	//	std::cout << "std " << ex.what() << std::endl;
-	//	return 1;
-	//}
-	//catch (...)
-	//{
-	//	std::cout << "UNRECOGNIZED EXCEPTION" << std::endl;
-	//	return 1;
-	//}
-	
-
-	//alternative method using the command line tools
-	//xtina throwback tbh
-
-	//navigate to setup for cmc forward simulation
-	//run forward dynamics tool from command line
-	//excitiations are changed using the control file
-	//change this for your machine
-	//system("cd E:/Dokumente/Schule/tum/Informatik/6/Bachelor-Arbeit/MoBL_ARMS_OpenSim33tutorial/ModelFiles/InputFiles_module7_CMCDrivenFowardSimulation && forward -S MoBL_ARMS_module7_FDS_Setup.xml");
-	
-	//do something with the results
-	//open states file
-	std::ifstream results;
-	std::string head;
-	std::string line;
-	line.reserve(4200);//length of one line, for better performance
-	results.open("E:/Dokumente/Schule/tum/Informatik/6/Bachelor-Arbeit/MoBL_ARMS_OpenSim33tutorial/ModelFiles/InputFiles_module7_CMCDrivenFowardSimulation/output/FDS_states.sto");
-
-	//do stuff with the results
-
-	//skip first two lines of header and read number of rows
-	std::getline(results, head);
-	std::getline(results, head);
-	std::getline(results, head);//correct line -> line 3
-	//get number
-	int rows = std::stoi(head.substr(6, 8));
-
-	//////////////////////////////////////////////////
-	//get line with the names of the values -> line 7
-	//might as well make this hardcoded, idc
-	std::getline(results, head);
-	std::getline(results, head);
-	std::getline(results, head);
-	std::getline(results, head); //line with column names
-
-	//explode string -> make method out of this
-	std::vector<std::string> names;
-	std::istringstream iss(head);
-	int k = 0;
-	for (std::string token; std::getline(iss, token, '\t'); ) {
-		names.push_back(std::move(token));
-		k++;
+		std::cout << "t1 : " << t1 << "\n" << "t2 : " << t2 << "\n" << "t3 : " << t3  << "\n" << "t4 : " << t4 << std::endl;
 	}
-
-	//filter for values you actually want
-	//-> we can set elv_angle, shoulder_elv, shoulder_rot, elbow_flexion, pro_sup, deviation, flexion
-	//remember positions
-	std::vector<int> positions(7); //in the order mentioned above
-
-	for (int i = 0; i < k; i++) {
-		//make a switch statement with string2int method for more sophistication and some kind of readability
-		std::string s = names[i];
-		if (s == "elv_angle") {
-			positions[0] = i;
-		}
-		else
-		{
-			if (s == "shoulder_elv") {
-				positions[1] = i;
-			}
-			else
-			{
-				if (s == "shoulder_rot") {
-					positions[2] = i;
-				}
-				else
-				{
-					if (s == "elbow_flexion") {
-						positions[3] = i;
-					}
-					else
-					{
-						if (s == "pro_sup") {
-							positions[4] = i;
-						}
-						else
-						{
-							if (s == "deviation") {
-								positions[5] = i;
-							}
-							else
-							{
-								if (s == "flexion") {
-									positions[6] = i;
-								}
-							}
-						}
-					}
-				}
-			}
-		}
+	catch (OpenSim::Exception ex)
+	{
+		std::cout << "opensim " << ex.getMessage() << std::endl;
+		return 1;
 	}
-	//////////////////////////////////////////////////
-
-	//go to last line of file = final state
-	//go down "rows", since we are at the beginning of the data now
-	for (int l = 0; l < rows; l++) {
-		std::getline(results, line);
+	catch (std::exception ex)
+	{
+		std::cout << "std " << ex.what() << std::endl;
+		return 1;
 	}
-	//again, explode this string = data
-	std::vector<std::string> data(7);
-	std::istringstream issastring(line);
-	for (std::string token; std::getline(issastring, token, '\t'); ) {
-		data.push_back(std::move(token));
+	catch (...)
+	{
+		std::cout << "UNRECOGNIZED EXCEPTION" << std::endl;
+		return 1;
 	}
-	//now output only the relevant values = things where we know the positions from above
-	for (int c = 0; c < 7; c++) {
-		std::cout << names[positions[c]] << " : " << data[positions[c]] << std::endl;
-	}
-
-
-	//close file
-	results.close();
-
-
-	std::cout << "mic drop\n" << std::endl;
-
 }
